7e5f3e57d8c5e2c92cafa239b56bab5c2fe95e79
孤鸿渺影 <iyanghong@foxmail.com>
2023-12-20 11:22:04 +0800

    新增权限组

---
+++ clever-common/src/main/java/com/clever/annotation/AuthGroup.java
@@ -0,0 +1,38 @@
+package com.clever.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 权限组
+ *
+ * @Author xixi
+ * @Date 2023-12-20 10:58
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthGroup {
+
+    /**
+     * 权限组名
+     *
+     * @return 名
+     */
+    String name();
+
+    /**
+     * 权限组描述
+     *
+     * @return 描述
+     */
+    String description() default "";
+
+    /**
+     * 所属服务
+     *
+     * @return 所属服务
+     */
+    String service() default "";
+}
