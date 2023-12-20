18147c829f0cfa40fef9ffb2b5e760dc46b2a677
孤鸿渺影 <iyanghong@foxmail.com>
2023-12-20 09:08:16 +0800

    新增全局异常拦截

--- clever-common/src/main/java/com/clever/exception/ConstantException.java
+++ clever-common/src/main/java/com/clever/exception/ConstantException.java
@@ -26,8 +26,11 @@ public class ConstantException {
         return Result.ofFail(code,message);
     }
 
+
+
     public static ConstantException USER_NO_ONLINE = new ConstantException(1001, "用户未登录");
     public static ConstantException USER_PASSWORD_ERROR_SO_MANY = new ConstantException(1002, "密码错误次数过多,30分钟后再试");
     public static ConstantException USER_ACCOUNT_NOT_FOUND = new ConstantException(1003, "账号不存在");
     public static ConstantException USER_LOGIN_PASSWORD_ERROR = new ConstantException(1004, "账号或密码不正确");
+    public static ConstantException PARAMETER_VERIFICATION_FAIL = new ConstantException(1005, "参数校验失败");
 }
--- clever-system/src/main/java/com/clever/config/BeanConfigurer.java
+++ clever-system/src/main/java/com/clever/config/BeanConfigurer.java
@@ -4,8 +4,13 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.clever.config.mybatis.ModelFieldAutoFillHandler;
+import org.hibernate.validator.HibernateValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 /**
  * @Author xixi
@@ -28,6 +33,17 @@ public class BeanConfigurer {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+
+    /**
+     * 配置参数验证器为快速失败模式
+     *
+     * @return 参数验证器
+     */
+    @Bean
+    public Validator getValidator() {
+        ValidatorFactory factory = Validation.byProvider(HibernateValidator.class).configure().failFast(true).buildValidatorFactory();
+        return factory.getValidator();
     }
 
 
