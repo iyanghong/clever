062b8cf9e1d7c2e95f42d1ab1aedf2ca367075e3
孤鸿渺影 <iyanghong@foxmail.com>
2023-12-20 17:52:24 +0800

    新增SystemConfigConstant

--- clever-system/src/main/java/com/clever/SystemConfigConstant.java
+++ clever-system/src/main/java/com/clever/SystemConfigConstant.java
@@ -13,4 +13,31 @@ public class SystemConfigConstant {
      * 用户登陆最大密码连续错误次数
      */
     public static final String USER_LOGIN_MAX_ERROR_COUNT = "sys:config:user:passwordMaxErrorNum";
+    /**
+     * 用户默认密码
+     */
+    public static final String USER_DEFAULT_PASSWORD = "sys:config:user:defaultPassword";
+    /**
+     * 用户默认登录过期时间
+     */
+    public static final String USER_DEFAULT_LOGIN_EXPIRE_TIME = "sys:config:user:defaultLoginExpireTime";
+
+    /**
+     * 用户默认角色
+     */
+    public static final String USER_DEFAULT_ROLE = "sys:config:user:defaultRole";
+
+    /**
+     * 用户默认头像
+     */
+    public static final String USER_DEFAULT_AVATAR = "sys:config:user:defaultAvatar";
+    /**
+     * 是否开启邮箱注册
+     */
+    public static final String USER_REGISTER_EMAIL_ENABLE = "sys:config:user:registerEmailEnable";
+    /**
+     * 是否开启手机注册
+     */
+    public static final String USER_REGISTER_PHONE_ENABLE = "sys:config:user:registerPhoneEnable";
+
 }
