8ff4e6f4ff2b13ed4ed764a5886054d002207e61
孤鸿渺影 <iyanghong@foxmail.com>
2024-01-09 09:45:06 +0800

    修改脚本

--- clever.sh
+++ clever.sh
@@ -12,6 +12,11 @@ serviceList=("system" "blog")
 
 
 build(){
+  checkDockerContainerIsRunning install-clever
+  if [ $? -eq 0 ]; then
+      echo -e "\033[32m 启动编译容器 \033[0m"
+      docker-compose -f "${projectPath}/docker-composer.yml" up -d
+  fi
   echo -e "\033[32m [${actionService}]开始编译 \033[0m"
   # "${actionServicePath}" || exit
   #mvn clean package -T 1C -Dmaven.test.skip=true -Dmaven.compile.fork=true
@@ -44,9 +49,9 @@ deploy(){
   # 停止正在运行的容器
   echo -e "\033[32m 停止正在运行的容器 \033[0m"
   docker-compose -f "${actionServicePath}/docker-composer.yml" down
-  docker rm -f clever-$actionService
-  docker rmi clever-$actionService
-#  docker build -t clever-$actionService .
+  docker rm -f clever-"$actionService"
+  docker rmi clever-"$actionService"
+  docker build -t clever-"$actionService" "$projectPath"
   # 强制重启正在运行的容器
   echo -e "\033[32m 启动容器 \033[0m"
   docker-compose -f "${actionServicePath}/docker-composer.yml" up -d --force-recreate
